3fb16f98b83e6808369d4fcea2a708ee505f59a8
Gia Edgington <gia.edgington@gmail.com>
2020-04-18 20:07:18 -0400

    Implement token validation

--- middleware/is-auth.js
+++ middleware/is-auth.js
@@ -1,19 +1,27 @@
 const jwt = require('jsonwebtoken');
 
 module.exports = (req, res, next) => {
-    const token = req.get('Authorization').split(' ')[1];
-    let decodedToken;
-    try {
-        decodedToken = jwt.verify(token, 'deepsecret');
-    } catch(err){
-        err.statusCode = 500;
-        throw err;
-    }
-    if(!decodedToken) {
-        const error = new Error('Not authenticated.');
-        error.statusCode = 401;
-        throw error;
-    }
-    req.userId = decodedToken;
-    next();
-}
+  const authHeader = req.get('Authorization');
+  if (!authHeader) {
+    const error = new Error('Not authenticated.');
+    error.statusCode = 401;
+    throw error;
+  }
+  const token = authHeader.split(' ')[1];
+  
+  let decodedToken;
+  try {
+    //console.log(token);
+    decodedToken = jwt.verify(token, 'deepsecret');
+  } catch (err) {
+    err.statusCode = 500;
+    throw err;
+  }
+  if (!decodedToken) {
+    const error = new Error('Not authenticated.');
+    error.statusCode = 401;
+    throw error;
+  }
+  req.userId = decodedToken.userId;
+  next();
+};
